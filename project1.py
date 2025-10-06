79827601e5d215b8212b2320a75a01429d6b1adc
Sabina <bsabina@umich.edu>
2025-10-06 00:21:28 -0400

    complete read_file function & store data in list of dictionaries

---
+++ project1.py
@@ -0,0 +1,34 @@
+import csv 
+
+def read_file(file_name):
+    try:
+        with open(file_name, "r", newline = "") as file:
+            reader = csv.DictReader(file)
+            penguin_list = []
+
+            for row in reader:
+                if 'NA' in row.values():
+                    continue
+
+                inner_d = {
+                    "species": row["species"],
+                    "island": row["island"],
+                    "bill_length_mm": float(row["bill_length_mm"]),
+                    "bill_depth_mm": float(row["bill_depth_mm"]),
+                    "flipper_length_mm": float(row["flipper_length_mm"]),
+                    "body_mass_g": float(row["body_mass_g"]),
+                    "sex": row["sex"],
+                    "year": int(row["year"])
+                }
+
+                penguin_list.append(inner_d)
+            return penguin_list
+    
+    except:
+        print("Failed to open file. Check for valid file name.")
+
+penguin = read_file("penguins.csv")
+print(penguin)
+
+
+
